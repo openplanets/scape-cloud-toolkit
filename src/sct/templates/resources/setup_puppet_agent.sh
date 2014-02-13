119b71f673681a988d20ab7de11966375ce204fd
Marian Neagul <marian@info.uvt.ro>
2014-02-13 23:51:52 +0200

    Enable swap on normal nodes

--- src/sct/templates/resources/setup_puppet_agent.sh
+++ src/sct/templates/resources/setup_puppet_agent.sh
@@ -10,3 +10,8 @@ echo -e "START=yes\nDAEMON_OPTS=\"\"\n" > /etc/default/puppet
 
 rm -fr /var/lib/puppet/ssl/*
 /etc/init.d/puppet start
+
+SWAP_DISKS=$(blkid -s TYPE | grep -i swap | cut -d ":" -f 1)
+for DSK in $SWAP_DISKS; do
+    swapon $DSK
+done
